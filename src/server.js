a0d5e7cd263928d904e75467a05072d1974768f3
Carolina velez <carolina.velez@mercadolibre.com.co>
2021-09-01 19:36:06 -0500

    modificar el server

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname+'/dist/monitorLatencia'));
 app.get('/',function(req,res){
-    res.sendFile(path.join('/dist/monitorLatencia/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/monitorLatencia/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
